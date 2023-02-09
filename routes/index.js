5d97e36e2cb557ae979c761c35fcfbd504fbd7c6
axdevil <deb.devel@outlook.es>
2023-02-09 16:46:54 -0600

    Controlador para el login

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const loginController = require("../controllers/loginController")
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', loginController.index);
 
 module.exports = router;
