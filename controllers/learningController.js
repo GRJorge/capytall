7856c61260fb10a414beb4c2dccfe82d7c33b8a7
axdevil <deb.devel@outlook.es>
2023-02-09 18:00:20 -0600

    Muestra de usuarios

--- controllers/learningController.js
+++ controllers/learningController.js
@@ -4,7 +4,7 @@ var learningModel = require('../model/learningModel')
 module.exports = {
     index:function(req,res){
         learningModel.get(con,function(err,data){
-            res.render('learning',{user:data})
+            res.render('learning',{users:data})
         })
     }
 }
