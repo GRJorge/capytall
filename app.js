131853605e862a8a9eb95c3949d5212c7ca2e602
axdevil <deb.devel@outlook.es>
2023-03-01 12:50:25 -0600

    Ruta Home

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var userRouter = require('./routes/user')
+var userRouter = require('./routes/user');
+var homeRouter = require('./routes/home');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/user', userRouter);
+app.use('/home', homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
