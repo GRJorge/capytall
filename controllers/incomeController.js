c1f4225aa0517545a01dc5431f70fa568d6c199d
axdevil <deb.devel@outlook.es>
2023-03-19 00:50:09 -0600

    Modal de eliminacion y envio de url

--- controllers/incomeController.js
+++ controllers/incomeController.js
@@ -13,8 +13,6 @@ module.exports = {
 		}
 	},
 	insert:function(req,res){
-		console.log(req.body);
-
 		let errors = []
 
 		const body = req.body
--- public/javascripts/income.js
+++ public/javascripts/income.js
@@ -3,6 +3,9 @@ const date = new Date()
 const dayInput = document.querySelector('#day')
 const monthInput = document.querySelector('#month')
 
+const addModal = document.querySelector('#addModal')
+const deleteModal = document.querySelector('#deleteModal')
+
 //AGREGAR FECHA ACTUAL
 dayInput.value = date.getDate().toString().padStart(2,'0')
 monthInput.value = (date.getMonth() + 1).toString().padStart(2,'0')
@@ -17,11 +20,30 @@ dayInput.addEventListener('input',() => {
 })
 
 //MOSTRAR FORMULARIO DE NUEVO INGRESO
-const addModal = document.querySelector('#addModal')
 
 document.querySelector('#addBtn').addEventListener('click',() => {
 	addModal.style.display = 'flex'
+	deleteModal.style.display = 'none'
 })
 document.querySelector('#cancelAdd').addEventListener('click',() => {
 	addModal.style.display = 'none'
+})
+
+//MOSTRAR FORMULARIO DE ELIMINACION Y CAMBIO DE DATOS
+
+document.querySelectorAll('#deleteBtn').forEach(btn => {
+	btn.addEventListener("click",() => {
+		addModal.style.display = "none"
+		deleteModal.style.display = "flex"
+		
+		const id = btn.getAttribute("dataId")
+		const folio = btn.getAttribute("dataFolio")
+
+		document.querySelector('#formDelete').action = '/income/delete/' + id
+		document.querySelector('#deleteFolioModal').innerHTML = folio
+	})
+})
+
+document.querySelector('#cancelDelete').addEventListener("click",() => {
+	deleteModal.style.display = "none"
 })
