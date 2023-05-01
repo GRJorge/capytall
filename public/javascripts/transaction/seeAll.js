1e3425abbd8cb69429b2f83ba76c569bf5ed9d2f
axdevil <deb.devel@outlook.es>
2023-05-01 13:00:12 -0600

    Detalles esteticos

--- public/javascripts/transaction/seeAll.js
+++ public/javascripts/transaction/seeAll.js
@@ -1,13 +1,43 @@
 const deleteModal = document.querySelector('#deleteModal')
+const tbody = document.querySelector('tbody')
 
-document.querySelector('tbody').querySelectorAll('tr').forEach(tr => {
+tbody.querySelectorAll('tr').forEach(tr => {
     tr.addEventListener('click',() => {
-        deleteModal.style.display = 'flex'
-        deleteModal.querySelector('#formDelete').action = deleteModal.querySelector('#formDelete').action + tr.getAttribute('incomeId') + ',table'
+        if(tr.style.backgroundColor != 'rgb(140, 63, 166)'){
+            tbody.querySelectorAll('tr').forEach(tr2 => {
+                tr2.style.backgroundColor = '#373740'
+            })
+            tr.style.backgroundColor = '#8C3FA6'
+            deleteModal.classList.add('fadeIn')
+            deleteModal.style.display = 'flex'
+            deleteModal.querySelector('#deleteFolioModal').innerHTML = tr.getAttribute('folio')
+            deleteModal.querySelector('#formDelete').action = deleteModal.querySelector('#formDelete').action + tr.getAttribute('incomeId') + ',table'
+        }else{
+            tr.style.backgroundColor = 'inherit'
+            hideDeleteModal()
+        }
     })
 })
 
-// YOYAL DE LA TABLA
+document.querySelector('#cancelDelete').addEventListener('click',() => {
+    hideDeleteModal()
+
+    tbody.querySelectorAll('tr').forEach(tr => {
+        if(tr.style.backgroundColor == 'rgb(140, 63, 166)'){
+            tr.style.backgroundColor = '#373740'
+            return
+        }  
+    })
+})
+
+function hideDeleteModal(){
+    deleteModal.classList.remove('fadeIn')
+    deleteModal.classList.add('fadeOut')
+    setTimeout(() => {
+        deleteModal.style.display = 'none'
+    },200)
+}
+// TOTAL DE LA TABLA
 
 const table = document.querySelector('#table')
 
