7bec947296a01c35789157d9bc6440d589a0e150
axdevil <deb.devel@outlook.es>
2023-05-03 08:09:46 -0600

    Balance

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,7 +1,21 @@
 document.querySelectorAll('#table').forEach(table => {
-	var total = 0
+	let total = 0
 	table.querySelectorAll('#mount').forEach(mount => {
 		total += parseFloat(mount.textContent.replace('$',''))
 	})
 	table.querySelector('#total').innerHTML = 'Total: $' + total
 })
+
+var totalGeneral = 0
+
+document.querySelectorAll('#total').forEach(total => {
+	let mount = parseFloat(total.textContent.replace('Total: $',''))
+
+	if(totalGeneral == 0){
+		totalGeneral = mount
+	}else{
+		totalGeneral -= mount
+	}
+})
+
+document.querySelector('#balance').querySelector('span').innerHTML = "Balance: $" + totalGeneral
