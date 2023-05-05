60f9c43d989de21a25632c945dab6bd85ea25f06
axdevil <deb.devel@outlook.es>
2023-05-05 13:49:52 -0600

    Llenado de tabla

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,9 +1,20 @@
 const con = require('../config/connection')
 
 const searchModel = require('../model/searchModel')
+const global = require('../public/javascripts/global')
 
 module.exports = {
     index:function(req,res){
-        res.render('search/index')
+        if(global.userId > 0){
+            searchModel.get(con,function(err,data){
+                if(!err){
+                    res.render('search/index',{data: data})
+                }else{
+                    console.log(err)
+                }
+            })
+        }else{
+            res.redirect('/user')
+        }
     }
 }
--- model/searchModel.js
+++ model/searchModel.js
@@ -1,3 +1,7 @@
+const global = require('../public/javascripts/global')
+
 module.exports = {
-    
+    get:function(con,fun){
+        con.query('SELECT CONCAT(DAY(date),"/",MONTH(date),"/",YEAR(date)) AS date, folio, concept, category.name AS category, type, mount FROM transaction INNER JOIN category WHERE categoryFK=category.id AND transaction.userFK=? AND transaction.visible=1 ORDER BY date',[global.userId],fun)
+    }
 }
